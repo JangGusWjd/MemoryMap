b3331dffa90064958d31a790d284dc121b4b3d30
JangGusWjd <jjanghj3624@gmail.com>
2024-11-26 22:50:35 +0900

    feat : theme color 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        customCoral: '#FF6969',
+        customCoralLight: '#FF696915',
+        customBlue: '69A5FF',
+        customGray: '#929090',
+        customGrayLight: '#F1F1F1',
+      },
+      borderRadius: {
+        '15px': '15px',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
