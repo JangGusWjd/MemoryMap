831d57598fcbed37bf18059e91f05cab4320c0d0
JangGusWjd <jjanghj3624@gmail.com>
2024-12-04 09:37:57 +0900

    feat: 게시글, 프로필 컴포넌트 페이지에 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,17 @@
 import StoryList from '@/components/home/StoryList'
 import KaKaoMap from '@/components/map/kakaomap'
+import ArticlePost from '@/components/home/ArticlePost'
+import Profile from '@/components/home/Profile'
 
 export default function HomePage() {
   return (
     <>
       <StoryList />
-      <KaKaoMap />
+      <Profile />
+      <div className='flex flex-col gap-2 sm:flex-col md:flex-row lg:flex-row'>
+        <KaKaoMap />
+        <ArticlePost />
+      </div>
     </>
   )
 }
