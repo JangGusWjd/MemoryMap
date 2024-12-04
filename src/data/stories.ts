27255c0247507641c9d0de8b89b58d2b1ed31b58
JangGusWjd <jjanghj3624@gmail.com>
2024-12-04 09:36:21 +0900

    fix: id 값 추가

--- src/data/stories.ts
+++ src/data/stories.ts
@@ -1,37 +1,46 @@
 const stories = [
   {
+    id: 1,
     name: 'guswjd',
     img: 'https://i.pinimg.com/236x/ac/58/cc/ac58ccb126ce274b7f8656518e8060ba.jpg',
   },
   {
+    id: 2,
     name: 'dafmeklr',
     img: 'https://thumb.pann.com/tc_480/https://fimg5.pann.com/new/download.jsp?FileID=61527773',
   },
   {
+    id: 3,
     name: 'Wkdgus',
     img: 'https://i.pinimg.com/736x/53/7e/f5/537ef59499259ba707068742f91a10f8.jpg',
   },
   {
+    id: 4,
     name: 'amdswq.1213',
     img: 'https://i.pinimg.com/222x/1e/dd/87/1edd874063db3ddaa3ec486415e90af6.jpg',
   },
   {
+    id: 5,
     name: 'amfkal',
     img: 'https://i.pinimg.com/236x/a5/bf/16/a5bf1642e439cd9d1414f226e92480df.jpg',
   },
   {
+    id: 6,
     name: 'aemr_1212',
     img: 'https://i.pinimg.com/236x/6c/bb/8c/6cbb8c247f23da937264fc4f217fc342.jpg',
   },
   {
+    id: 7,
     name: 'akf_0000',
     img: 'https://i.pinimg.com/736x/5d/d7/b9/5dd7b9a36ca790c8b0f38bd43f1dacec.jpg',
   },
   {
+    id: 8,
     name: 'b.__.23123',
     img: 'https://i.pinimg.com/236x/0b/c8/8d/0bc88d20b83732068e530524b94faaa2.jpg',
   },
   {
+    id: 9,
     name: 'nflwekf',
     img: 'https://i.pinimg.com/236x/b6/f1/69/b6f169ad18b0fb94ba9e02cc18968d07.jpg',
   },
